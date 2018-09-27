6e9af27b0bb05aa35e9a9d5ab3c484b2acdcb958
Do Hoang Viet <d_h_viet@ga-tech.co.jp>
2018-09-27 08:09:12 +0700

    Improve login by username and password

---
+++ app/models/user.rb
@@ -0,0 +1,18 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable, :confirmable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  validates :username, presence: true, length: {minimum: 5, maximum: 20}, format: { with: /\A[A-z]*[0-9]*\z/ }
+
+  validate :check_valid_username
+
+  def check_valid_username
+    # Sample for add err
+    if username == 'robin'
+      # errors.add(:username, "too_long.other".to_sym, count: 10)
+      errors.add(:username, I18n.t('errors.messages.vh_error', count: username.length))
+    end
+  end
+end
