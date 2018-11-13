be790dcbd0f5457efa98e1dffe8d8eb563601fe6
VH <vh@Apples-MacBook-Pro.local>
2018-11-14 05:53:29 +0700

    Improve css for customer

--- db/schema.rb
+++ db/schema.rb
@@ -15,33 +15,10 @@ ActiveRecord::Schema.define(version: 2018_10_30_082230) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "active_storage_attachments", force: :cascade do |t|
-    t.string "name", null: false
-    t.string "record_type", null: false
-    t.bigint "record_id", null: false
-    t.bigint "blob_id", null: false
-    t.datetime "created_at", null: false
-    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
-    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
-  end
-
-  create_table "active_storage_blobs", force: :cascade do |t|
-    t.string "key", null: false
-    t.string "filename", null: false
-    t.string "content_type"
-    t.text "metadata"
-    t.bigint "byte_size", null: false
-    t.string "checksum", null: false
-    t.datetime "created_at", null: false
-    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
-  end
-
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "image"
-    t.string "pdf_file"
   end
 
   create_table "customers", force: :cascade do |t|
@@ -52,6 +29,22 @@ ActiveRecord::Schema.define(version: 2018_10_30_082230) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "customers_idx01"
+  end
+
+  create_table "my_logins", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "username", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "login_type", default: 0, null: false
+    t.index ["email"], name: "index_my_logins_on_email", unique: true
+    t.index ["login_type"], name: "index_my_logins_on_login_type"
+    t.index ["reset_password_token"], name: "index_my_logins_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_my_logins_on_username", unique: true
   end
 
   create_table "products", force: :cascade do |t|
